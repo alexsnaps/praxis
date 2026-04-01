758b85d6e06143ab4d9a855a4c7c82909c52ead8
Shane Utt <shaneutt@linux.com>
2026-04-01 16:27:35 -0400

    feat: add tests crate
    Signed-off-by: Shane Utt <shaneutt@linux.com>

---
+++ tests/integration/tests/suite/main.rs
@@ -0,0 +1,20 @@
+//! Integration test suite for Praxis.
+
+mod body;
+mod body_pipeline;
+mod common;
+mod compression;
+mod conditions;
+mod downstream_read_timeout;
+mod filter_composition;
+mod guardrails;
+mod ip_acl;
+mod json_body_field;
+mod payload_processing;
+mod per_listener_pipeline;
+mod retry;
+mod routing;
+mod security;
+mod tcp_access_log;
+mod tls;
+mod wildcard_routing;
