08478a5b53544b86d92ccba10f5198396804b9ac
Shane Utt <shaneutt@linux.com>
2026-09-23 21:01:43 -0400

    test(integration): validate every example config the way startup does
    Signed-off-by: Shane Utt <shaneutt@linux.com>

--- tests/integration/tests/suite/examples/mod.rs
+++ tests/integration/tests/suite/examples/mod.rs
@@ -10,6 +10,7 @@ pub use test_utils::load_example_config;
 mod access_log_fields;
 mod access_logging;
 mod admin_interface;
+mod all_examples_validate;
 mod api_key_filter;
 mod authority_override;
 mod basic_reverse_proxy;
